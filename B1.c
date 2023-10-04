d3b711d3d32cf38af5725cc576e6e119200093c7
mayank1133 <140825299+mayank1133@users.noreply.github.com>
2023-10-04 09:52:04 +0530

    Update B1.c

--- B1.c
+++ B1.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 void main() 
 {
-  int i, n,t1 = 0, t2 = 1,nextTerm = t1 + t2;
+  int i, n,t1 = 0, t2 = 1,next = t1 + t2;
   printf("Enter the number of terms: ");
   scanf("%d", &n);
   printf("Fibonacci Series: %d, %d, ", t1, t2);
   for (i = 3; i <= n; ++i) 
   {
-    printf("%d, ", nextTerm);
+    printf("%d, ", next);
     t1 = t2;
-    t2 = nextTerm;
-    nextTerm = t1 + t2;
+    t2 = next;
+    next = t1 + t2;
   }
 }
